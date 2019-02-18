b3e95d322255cd63edb6b886a1b53f7637693651
wagwagtail <32199017+wagwagtail@users.noreply.github.com>
2019-02-18 21:58:14 +0000

    bokeh update

--- analyser.py
+++ analyser.py
@@ -7,20 +7,26 @@ import requests
 
 class twitterquery:
 
-    def get_twitter_response(self):
-        api = tools.TwitterClient()
-        tweets = api.get_tweets(query='trump', count=250)
-        ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
-        ptweetsperc = (100 * len(ptweets) / len(tweets))
-        ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
-        ntweetsperc = (100 * len(ntweets) / len(tweets))
-        neuttweetperc = (100 * (len(tweets) - len(ntweets) - len(ptweets)) / len(tweets))
-        testtime = datetime.datetime.now()
+    def get_twitter_response():
+        try:
+            api = tools.TwitterClient()
+            tweets = api.get_tweets(query='USDJPY', count=1000)
+            ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+            ptweetsperc = (100 * len(ptweets) / len(tweets))
+            ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+            ntweetsperc = (100 * len(ntweets) / len(tweets))
+            neuttweetperc = (100 * (len(tweets) - len(ntweets) - len(ptweets)) / len(tweets))
+            testtime = datetime.datetime.now()
+        except:
+            ptweetsperc = 50
+            ntweetsperc = 50
+            neuttweetperc = 50
+            testtime = datetime.datetime.now()
         return (ptweetsperc, ntweetsperc, neuttweetperc, testtime)
 
 
 class oandaquery:
-    def stream(self):
+    def stream():
         headers = {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer 7988784158b97bb23e2c31e1c3c2963b-6760148dd294e502680d2eb6e94b0235',
@@ -30,34 +36,17 @@ class oandaquery:
             'https://api-fxpractice.oanda.com/v3/accounts/101-004-9972545-001/pricing?instruments=USD_JPY',
             headers=headers)
         prices = response.json()
-        bids = prices['prices'][0]['asks'][0]['price']
-        asks = prices['prices'][0]['bids'][0]['price']
-        return (bids, asks)
+        bids = float(prices['prices'][0]['asks'][0]['price'])
+        asks = float(prices['prices'][0]['bids'][0]['price'])
+        oandatime = datetime.datetime.now()
+        return bids, asks, oandatime
 
-    # if __name__ == "__main__":
-#
-#     for x in range(0, 600):
-#         print('working{}'.format(x))
-#         newDF = pd.DataFrame()
-#         testtime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
-#
-#         for j in range(0, 200):
-#             df = pd.DataFrame()
-#             df['time'] = datetime.datetime.now()
-#             df = df.append(df['time'])
-#             vals = get_twitter_response()
-#             prices = stream()
-#             print(prices)
-#             df['asks'] = prices['prices'][0]['asks'][0]['price']
-#             df['bids'] = prices['prices'][0]['bids'][0]['price']
-#             df['positive'] = vals[0]
-#             df['negative'] = vals[1]
-#             df['neutral'] = vals[2]
-#             df['time'] = datetime.datetime.now()
-#             newDF = newDF.append(df)
-#             time.sleep(20)
-#             j += 1
-#
-#         newDF.to_csv("out{}.csv".format(testtime), index=False)
-#
-#     x += 1
+
+if __name__ == "__main__":
+    while True:
+        try:
+            prices = oandaquery.stream()
+            print(prices)
+            time.sleep(2)
+        except:
+            print("An exception occurred")
--- bokeh.py
+++ bokeh.py
@@ -3,48 +3,49 @@
 from analyser import twitterquery, oandaquery
 import datetime
 from bokeh.layouts import row, column, gridplot
-from bokeh.models import ColumnDataSource, Slider, Select
+from bokeh.models import LinearAxis, ColumnDataSource, Range1d, Slider, Select
 from bokeh.plotting import curdoc, figure
 from bokeh.driving import count
 
 source = ColumnDataSource(dict(
-    time=[],
+    oandatime=[],
+    twittertime=[],
     PositiveTweets=[],
     NegativeTweets=[],
     priceask=[],
     pricebids=[]
 ))
 
-p = figure(plot_height=500, tools="xpan,xwheel_zoom,xbox_zoom,reset", x_axis_type="datetime", y_axis_location="right")
-p.x_range.follow = "end"
-p.x_range.follow_interval = 100
-p.x_range.range_padding = 0
-
-p.line(x='time', y='PositiveTweets', alpha=0.8, line_width=3, color='green', source=source)
-p.line(x='time', y='NegativeTweets', alpha=0.8, line_width=3, color='red', source=source)
-
-p2 = figure(plot_height=500, tools="xpan,xwheel_zoom,xbox_zoom,reset", x_axis_type="datetime", y_axis_location="right")
-p2.line(x='time', y='priceask', alpha=0.8, line_width=3, color='green', source=source)
-p2.line(x='time', y='pricebids', alpha=0.8, line_width=3, color='red', source=source)
+p = figure(plot_height=300, x_axis_type="datetime", y_axis_location="right")
 
 
+p.line(x='twittertime', y='PositiveTweets', alpha=0.8, line_width=3, color='green', source=source)
+p.line(x='twittertime', y='NegativeTweets', alpha=0.8, line_width=3, color='red', source=source)
 
-@count()
+p2 = figure(plot_height=300, x_axis_type="datetime", y_axis_location="right")
+p2.line(x='oandatime', y='priceask', alpha=0.8, line_width=3, color='green', source=source)
+p2.line(x='oandatime', y='pricebids', alpha=0.8, line_width=3, color='red', source=source)
+
+@ count()
+
+
 def update(t):
-    positiveteets, negativetweets, neutraltweets, testtime = twitterquery.get_twitter_response(t)
-    asks, bids = oandaquery.stream(t)
+    positiveteets, negativetweets, neutraltweets, twittertime = twitterquery.get_twitter_response()
+    asks, bids, oandatime = oandaquery.stream()
 
     new_data = dict(
-        time=[testtime],
+        twittertime=[twittertime],
+        oandatime=[oandatime],
         PositiveTweets=[positiveteets],
         NegativeTweets=[negativetweets],
         priceask=[asks],
         pricebids=[bids]
     )
 
-    source.stream(new_data, 300)
+    source.stream(new_data)
 
 
 curdoc().add_root(gridplot([[p],[p2]], plot_width=2000))
-curdoc().add_periodic_callback(update, 5000)
-curdoc().title = "brexit"
+p.x_range = p2.x_range
+curdoc().add_periodic_callback(update, 7000)
+curdoc().title = "test"
